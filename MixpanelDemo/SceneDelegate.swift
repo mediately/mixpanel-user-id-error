7d87e80e30942ec90f4ec397584e6d3b1d2fe656
Marko Engelman <marko.engelman@mediately.co>
2022-12-06 10:18:18 +0100

    Setup 'AnalyticsEngine'.

--- MixpanelDemo/SceneDelegate.swift
+++ MixpanelDemo/SceneDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
+    var engine: AnalyticsEngine?
     var window: UIWindow?
     
     
@@ -16,7 +16,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        engine = AnalyticsEngine()
+        engine?.instantiate()
+        
+        guard let viewController = scene.windows.first?.rootViewController as? ViewController else { return }
+        viewController.engine = engine
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
--- MixpanelDemo/ViewController.swift
+++ MixpanelDemo/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var engine: AnalyticsEngine?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
