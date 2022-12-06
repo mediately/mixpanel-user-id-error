234b85f352d042330506a5eb638d7cc4a5bb65db
Marko Engelman <engelman.marko@gmail.com>
2022-12-06 10:10:17 +0100

    Initial Commit

---
+++ MixpanelDemo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MixpanelDemo
+//
+//  Created by Marko Engelman on 06/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
