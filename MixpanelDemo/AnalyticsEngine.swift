64ad373bd64325bd4c597181918db0c9ee62ad0f
Marko Engelman <marko.engelman@mediately.co>
2022-12-06 10:15:28 +0100

    Add 'AnalyticsEngine'.

---
+++ MixpanelDemo/AnalyticsEngine.swift
@@ -0,0 +1,14 @@
+//
+//  AnalyticsEngine.swift
+//  MixpanelDemo
+//
+//  Created by Marko Engelman on 06/12/2022.
+//
+
+import Foundation
+
+final class AnalyticsEngine {
+    func instantiate() {
+        
+    }
+}
